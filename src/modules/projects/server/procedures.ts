68e9583a3c8aac730faa5d93d7556fb319b51338
Sahib-nx <sahibyar214@gmail.com>
2025-07-12 15:12:34 +0530

    10:  messages ui

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,8 +3,28 @@ import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import z from "zod";
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectRouter = createTRPCRouter({
+    getOne: baseProcedure
+        .input(
+            z.object({
+                id: z.string().min(1, { message: "Id is required" })
+            })
+        )
+        .query(async ({ input }) => {
+            const existingProject = await prisma.project.findUnique({
+                where: {
+                    id: input.id,
+                },
+            });
+
+            if(!existingProject) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Project not found"})
+            }
+
+            return existingProject;
+        }),
     getMany: baseProcedure
         .query(async () => {
             const projects = await prisma.project.findMany({
@@ -19,12 +39,12 @@ export const projectRouter = createTRPCRouter({
         .input(
             z.object({
                 value: z.string()
-                .min(1, { message: "Value is required" })
-                .max(1000, {message: "Value is too long"})
+                    .min(1, { message: "Value is required" })
+                    .max(1000, { message: "Value is too long" })
             }),
         )
         .mutation(async ({ input }) => {
-           const createdProject = await prisma.project.create({
+            const createdProject = await prisma.project.create({
                 data: {
                     name: generateSlug(2, {
                         format: "kebab"
